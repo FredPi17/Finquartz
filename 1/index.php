181ddcb90981918cc42cb4c3536d6ca482c76d9f
Frédéric <fpinaud17@gmail.com>
2017-06-23 14:42:33 +0200

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Finquartz</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.0/semantic.min.css" />
+
+  </head>
+  <body>
+
+    <style>
+      .brik{
+        border: 1px solid black;
+      }
+    </style>
+    <div class="ui main container" id="menu">
+      <div class="row">
+        <div class="callout">
+          <div class="row">
+            <div class="large-12 columns">
+              <div class="large-6 columns">
+                <h1>Finquartz</h1>
+              </div>
+              <div class="large-2 columns">
+                <label>Discover</label>
+                <select>
+                  <option value="action1">action1</option>
+                  <option value="action2">Action2</option>
+                  <option value="action3">Action3</option>
+                </select>
+              </div>
+              <div class="large-2 columns">
+                <label>Action</label>
+                <select>
+                  <option value="pdf" id="create_pdf">.pdf</option>
+                  <option value="mail" id="create_mail">Mail</option>
+                  <option value="html" id="create_html">HTML</option>
+                </select>
+              </div>
+              <div class="large-2 columns">
+                <label>Profil</label>
+                <select>
+                  <option value="profile"><a href="profil.html">Profile</a></option>
+                  <option value="preferences"><a href="preferences.html">Préférences</a></option>
+                  <option value="logout"><a href="logout()">Logout</a></option>
+                </select>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-12 columns">
+          <div class="large-2 columns">
+            <label>Portfolio ABC</label>
+            <select>
+              <option value="portfolio1">portfolio1</option>
+              <option value="portfolio2">portfolio2</option>
+              <option value="portfolio3">portfolio3</option>
+            </select>
+          </div>
+          <div class="large-4 columns">
+            <label>Select time</label>
+            <textarea placeholder="small-12.columns"></textarea>
+          </div>
+          <div class="large-1 columns">
+            <button class="secondary button">1D</button>
+          </div>
+          <div class="large-1 columns">
+            <button class="secondary button">1W</button>
+          </div>
+          <div class="large-1 columns">
+            <button class="secondary button">MTD</button>
+          </div>
+          <div class="large-1 columns">
+            <button class="secondary button">YTD</button>
+          </div>
+          <div class="large-2 columns">
+            <label>New stats box</label>
+            <textarea placeholder="small-12.columns"></textarea>
+          </div>
+        </div>
+      </div>
+        <div class="row">
+          <div class="small-6 columns">
+            <div class="small-3 columns">
+              <label>Companies</label>
+            </div>
+            <div class="small-3 columns">
+              <label>Comment</label>
+            </div>
+            <div class="small-3 columns">
+              <label>Graph</label>
+            </div>
+            <div class="small-3 columns">
+              <label>Macro</label>
+            </div>
+          </div>
+            <div class="small-4 columns">
+              <label></label>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div id="body">
+    <div class="row">
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+          <div class="small-12 columns">
+            <h3>APPLE COMPUTER</h3>
+            <p>USD 520 +84%</p>
+            <img src="img/test.png" class="test" id="test"></img>
+            <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+            <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+          </div>
+          <div class="small-2 columns">
+            <p>26 SIMILAR NEWS</p>
+          </div>
+          <div class="small-2 columns">
+            <p>SHARE</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+          <div class="small-2 columns">
+            <p>3 minute read</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+          <div class="small-12 columns">
+            <h3>APPLE COMPUTER</h3>
+            <p>USD 520 +84%</p>
+            <img src="img/test.png" class="test" id="test"></img>
+            <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+            <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+          </div>
+          <div class="small-2 columns">
+            <p>26 SIMILAR NEWS</p>
+          </div>
+          <div class="small-2 columns">
+            <p>SHARE</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+          <div class="small-2 columns">
+            <p>3 minute read</p>
+          </div>
+          <div class="small-2 columns">
+            <p>logo</p>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+            <div class="small-12 columns">
+              <h3>APPLE COMPUTER</h3>
+              <p>USD 520 +84%</p>
+              <img src="img/test.png" class="test" id="test"></img>
+              <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+              <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+            </div>
+            <div class="small-2 columns">
+              <p>26 SIMILAR NEWS</p>
+            </div>
+            <div class="small-2 columns">
+              <p>SHARE</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>3 minute read</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+            <div class="small-12 columns">
+              <h3>APPLE COMPUTER</h3>
+              <p>USD 520 +84%</p>
+              <img src="img/test.png" class="test" id="test"></img>
+              <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+              <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+            </div>
+            <div class="small-2 columns">
+              <p>26 SIMILAR NEWS</p>
+            </div>
+            <div class="small-2 columns">
+              <p>SHARE</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>3 minute read</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+            <div class="small-12 columns">
+              <h3>APPLE COMPUTER</h3>
+              <p>USD 520 +84%</p>
+              <img src="img/test.png" class="test" id="test"></img>
+              <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+              <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+            </div>
+            <div class="small-2 columns">
+              <p>26 SIMILAR NEWS</p>
+            </div>
+            <div class="small-2 columns">
+              <p>SHARE</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>3 minute read</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="small-4 columns">
+        <div class="callout">
+          <div class="row">
+            <div class="small-12 columns">
+              <h3>APPLE COMPUTER</h3>
+              <p>USD 520 +84%</p>
+              <img src="img/test.png" class="test" id="test"></img>
+              <h6>Apple reports strong fiscl 4th Quarter Earnings<h6>
+              <p>Forbes.com OVT 25, 2016 @ 06:23PM</p>
+            </div>
+            <div class="small-2 columns">
+              <p>26 SIMILAR NEWS</p>
+            </div>
+            <div class="small-2 columns">
+              <p>SHARE</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+            <div class="small-2 columns">
+              <p>3 minute read</p>
+            </div>
+            <div class="small-2 columns">
+              <p>logo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.js"></script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdn.rawgit.com/MrRio/jsPDF/master/dist/jspdf.min.js"></script>
+    <script type="text/javascript" src="//cdn.rawgit.com/niklasvh/html2canvas/0.5.0-alpha2/dist/html2canvas.min.js"></script>
+  </body>
+</html>
